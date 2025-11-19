22bdea21e1c98ab666e5357e1cb3555ddf8a0103
UncouthClodhopper <96358734+UncouthClodhopper@users.noreply.github.com>
2025-11-19 14:30:02 -0600

    stuff and stuff

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './structural/App';
 
 createRoot(document.getElementById('root')).render(
     <App />
