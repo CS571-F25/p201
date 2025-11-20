4256e3c01f7962a8d47d3b068f93c95ee753672f
UncouthClodhopper <96358734+UncouthClodhopper@users.noreply.github.com>
2025-11-19 20:35:39 -0600

    yeah yeah yeah

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import './index.css'
 import App from './structural/App';
 import ReactDOM from 'react-dom/client'
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
     <App />
 )
